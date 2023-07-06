7586e5689c0500e5dea379b813a09ca202627a6d
Sandeep <sandeepiitm2003@gmail.com>
2023-07-06 11:38:06 +0530

    all working fine

--- index.js
+++ index.js
@@ -35,11 +35,18 @@ const checkWin = () =>{
     wins.forEach(e=>{
         if((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText)&& (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) &&(boxtexts[e[0]].innerText !== '') ){
             document.querySelector('.info').innerText = boxtexts[e[0]].innerText+ " has Won";
+            // boxtexts[e].style.backGround = "red";
+            console.log(boxtexts[e[0]].parentNode);
+            // boxtexts[e[0]].parentNode.style.border = "2px solid red";
+            boxtexts[e[0]].parentNode.style.backgroundColor = "#FF8C8C";
+            boxtexts[e[1]].parentNode.style.backgroundColor = "#FF8C8C";
+            boxtexts[e[2]].parentNode.style.backgroundColor = "#FF8C8C";
+            console.log("hello")
            
             isgameover = true; 
             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = '200px';    
-            document.querySelector('.line').style.width = "20vw";
-            document.querySelector('.line').style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+            // document.querySelector('.line').style.width = "20vw";
+            // document.querySelector('.line').style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
 
             
          
@@ -87,6 +94,13 @@ Array.from(boxes).forEach(element =>{
 
 //add onclick listener to the reset button
 reset = document.querySelector('#reset');
+
+
+
+
+
+
+let boxtexts = document.getElementsByClassName("boxtext");
 reset.addEventListener('click', ()=>{
     let boxtexts = document.querySelectorAll('.boxtext');
     Array.from(boxtexts).forEach(element =>{
@@ -94,7 +108,24 @@ reset.addEventListener('click', ()=>{
     });
     turn = "X";
     isgameover = false;
-    document.querySelector('.line').style.width = "0vw";
+    // document.querySelector('.line').style.width = "0vw";
+
+    let wins = [
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
+    ]
+    wins.forEach(e=>{
+        boxtexts[e[0]].parentNode.style.backgroundColor = "transparent";
+        boxtexts[e[1]].parentNode.style.background = "transparent";
+        boxtexts[e[2]].parentNode.style.background = "transparent";
+    
+    });
     document.getElementsByClassName("info")[0].innerText = "Turn for "+ turn;
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px";
     
